20e608fe8fbf11cf88710a58a48ccd325f9d80aa
Joshua Furman <joshua.furman@charter.com>
2023-02-21 14:29:53 -0700

    Grab all workspaces and data

--- main.go
+++ main.go
@@ -5,53 +5,73 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/olekukonko/tablewriter"
 )
 
-func main() {
-	// // Read the file
-	// json_data, err := ioutil.ReadFile("show_tenant")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-
-	// Must set ENV for path to terraform binary
-	terraform_binary_path := os.Getenv("TFB_PATH")
-	if terraform_binary_path == "" {
-		fmt.Println("Must set TFB_PATH Environment variable for path to terraform binary.")
-	}
-
-	cmd := exec.Command(string(terraform_binary_path), "show", "-json")
+func get_workspaces(binary_path string) []string {
+	// Execute terraform command for getting all workspaces
+	cmd := exec.Command(string(binary_path), "workspace", "list")
 	stdout, err := cmd.Output()
-
 	if err != nil {
 		fmt.Println(err.Error())
-		return
+		return nil
 	}
 
-	// Parse the JSON
-	var data map[string]interface{}
-	err = json.Unmarshal(stdout, &data)
-	if err != nil {
-		fmt.Println(err)
-		return
+	lines := strings.Split(string(stdout), "\n")
+
+	// Remove the * character, strip whitespace from the lines, and skip blank lines.
+	var workspaces []string
+	for _, line := range lines {
+		if strings.Contains(line, "*") {
+			line = strings.ReplaceAll(line, "*", "")
+		}
+		line = strings.TrimSpace(line)
+		if line != "" {
+			workspaces = append(workspaces, line)
+		}
 	}
 
-	// Store hostname and IP
+	// Return list of workspaces
+	return workspaces
+}
+
+func deployment_data(binary_path string, workspaces []string) [][]string {
+	// Store workspace, hostname and IP
 	var machines [][]string
 
-	for _, child := range data["values"].(map[string]interface{})["root_module"].(map[string]interface{})["child_modules"].([]interface{})[1].(map[string]interface{})["child_modules"].([]interface{})[0].(map[string]interface{})["child_modules"].([]interface{}) {
-		for _, resource := range child.(map[string]interface{})["resources"].([]interface{}) {
-			if values, ok := resource.(map[string]interface{})["values"].(map[string]interface{}); ok {
-				if clone, ok := values["clone"].([]interface{}); ok && len(clone) > 0 {
-					if customize, ok := clone[0].(map[string]interface{})["customize"].([]interface{}); ok && len(customize) > 0 {
-						if linuxOptions, ok := customize[0].(map[string]interface{})["linux_options"].([]interface{}); ok && len(linuxOptions) > 0 {
-							if hostName, ok := linuxOptions[0].(map[string]interface{})["host_name"].(string); ok {
-								if networkInterface, ok := customize[0].(map[string]interface{})["network_interface"].([]interface{}); ok && len(networkInterface) > 0 {
-									if ipv4Address, ok := networkInterface[0].(map[string]interface{})["ipv4_address"].(string); ok {
-										machines = append(machines, []string{hostName, ipv4Address})
+	for _, workspace := range workspaces {
+		// Change workspace
+		workspace_cmd := exec.Command(string(binary_path), "workspace", "select", workspace)
+		_, err := workspace_cmd.Output()
+
+		output_cmd := exec.Command(string(binary_path), "show", "-json")
+		stdout, err := output_cmd.Output()
+		if err != nil {
+			fmt.Println(err.Error())
+			return nil
+		}
+
+		// Parse the JSON
+		var data map[string]interface{}
+		err = json.Unmarshal(stdout, &data)
+		if err != nil {
+			fmt.Println(err)
+			return nil
+		}
+
+		for _, child := range data["values"].(map[string]interface{})["root_module"].(map[string]interface{})["child_modules"].([]interface{})[1].(map[string]interface{})["child_modules"].([]interface{})[0].(map[string]interface{})["child_modules"].([]interface{}) {
+			for _, resource := range child.(map[string]interface{})["resources"].([]interface{}) {
+				if values, ok := resource.(map[string]interface{})["values"].(map[string]interface{}); ok {
+					if clone, ok := values["clone"].([]interface{}); ok && len(clone) > 0 {
+						if customize, ok := clone[0].(map[string]interface{})["customize"].([]interface{}); ok && len(customize) > 0 {
+							if linuxOptions, ok := customize[0].(map[string]interface{})["linux_options"].([]interface{}); ok && len(linuxOptions) > 0 {
+								if hostName, ok := linuxOptions[0].(map[string]interface{})["host_name"].(string); ok {
+									if networkInterface, ok := customize[0].(map[string]interface{})["network_interface"].([]interface{}); ok && len(networkInterface) > 0 {
+										if ipv4Address, ok := networkInterface[0].(map[string]interface{})["ipv4_address"].(string); ok {
+											machines = append(machines, []string{workspace, hostName, ipv4Address})
+										}
 									}
 								}
 							}
@@ -62,15 +82,35 @@ func main() {
 		}
 	}
 
-	// Print the hostname and IPs
-	if len(machines) == 0 {
-		fmt.Println("No data found")
+	return machines
+}
+
+func main() {
+
+	// Must set ENV for path to terraform binary
+	terraform_binary_path := os.Getenv("TFB_PATH")
+	if terraform_binary_path == "" {
+		fmt.Println("Must set TFB_PATH Environment variable for path to terraform binary.")
+	}
+
+	// Get all workspaces
+	workspaces := get_workspaces(terraform_binary_path)
+	if workspaces == nil {
+		fmt.Println("Could not get workspaces from terraform.")
+		return
+	}
+
+	// Populate machines data
+	machines := deployment_data(terraform_binary_path, workspaces)
+	if machines == nil {
+		fmt.Println("Something went wrong getting workspace data.")
 		return
 	}
 
 	// Build table for data
 	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{"HostName", "IP Address"})
+	table.SetHeader([]string{"Workspace", "HostName", "IP Address"})
+	table.SetAutoMergeCells(true)
 	table.SetRowLine(true)
 
 	for _, v := range machines {
@@ -78,20 +118,5 @@ func main() {
 	}
 
 	table.Render()
-
-	// workingDir := "/home/jfurman/istio/caas/terraform/config/vsphere"
-	// execPath := "/home/jfurman/bin/terraform1.1.7"
-	// tf, err := tfexec.NewTerraform(workingDir, execPath)
-	// if err != nil {
-	// 	fmt.Printf("error running NewTerraform: %s\n", err)
-	// }
-
-	// state, err := tf.Show(context.Background())
-	// if err != nil {
-	// 	fmt.Printf("error running Show: %s", err)
-	// }
-
-	// fmt.Println(state.Values.RootModule.Resources)
-	// fmt.Printf("%T\n", state.Values)
 
 }
